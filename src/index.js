49e4cae13047f7fce5bbd0b633825ad96d9ea6c9
Joaquin Araujo <joaquin.araujo@bci.cl>
2022-09-22 10:00:33 -0300

    extra code was removed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 const path = require('path')
 const fs = require('fs').promises
-const Processor = require('./processor')
+const exec = require('child_process').exec
 const temporalSource = path.join(__dirname, './in-memory/doc.pdf')
 const temporalDestination = path.join(__dirname, './in-memory/doc-encrypted.pdf')
+
+class Processor {
+  constructor(
+    password,
+    username
+  ) {
+    this.password = password
+    this.username = username
+  }
+
+  async encrypt(orgfilepath, newfilepath) {
+    return this.process('encrypt', orgfilepath, newfilepath)
+  }
+
+  async process(method, orgfilepath, newfilepath) {
+    const encryptCommand = `qpdf --${method} ${this.username} ${this.password} 40 -- ${orgfilepath} ${newfilepath}`
+
+    return new Promise((resolve, reject) => {
+      exec(encryptCommand, error =>
+        error ?
+          reject({
+            error: true,
+            message: error
+          }) : resolve({
+            error: false,
+            message: `PDF ${method}ed successfully`
+          })
+      )
+    })
+  }
+}
 
 async function encryptPdf (base64Pdf, {
   username,
@@ -18,4 +49,19 @@ async function encryptPdf (base64Pdf, {
   return fs.readFile(temporalDestination, { encoding: 'base64' })
 }
 
-module.exports = encryptPdf
+async function handler ({
+  base64Pdf,
+  username,
+  password
+}) {
+  const encryptedBase64Pdf = await encryptPdf(base64Pdf, {
+    username,
+    password
+  })
+
+  return {
+    encryptedBase64Pdf
+  }
+}
+
+module.exports = handler
--- src/utils/saveBase64Pdf.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const fs = require('fs').promises
-
-async function saveBase64Pdf (base64Pdf) {
-  const bitmap = new Buffer.from(base64Pdf, 'base64')
-  return fs.writeFile(path.join(__dirname, '/doc-encrypted.pdf'), bitmap)
-}
-
-module.exports = saveBase64Pdf
